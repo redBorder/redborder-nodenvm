cbf56aa37510a27f039d58ac13a866c9cf72bf6e
Daniel Castro <dcastro@redborder.com>
2024-07-25 16:56:48 +0100

    Makefile changes and bring back download_modules

---
+++ packaging/download_modules.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NVM_VERSION=${NVM_VERSION:="0.39.1"}
+NODE_VERSION=${NODE_VERSION:="16.13.0"}
+NVM_DIR=${NVM_DIR:="/usr/lib/nvm"}
+
+mkdir -p SOURCES
+
+# Download nvm
+if [ ! -f SOURCES/nvm-${NVM_VERSION}.tar.gz ]; then
+  wget --no-check-certificate https://github.com/nvm-sh/nvm/archive/v${NVM_VERSION}.tar.gz -O SOURCES/nvm-${NVM_VERSION}.tar.gz
+fi
+
+# Download Node.js binary
+if [ ! -f SOURCES/node-v${NODE_VERSION}-linux-x64.tar.xz ]; then
+  wget --no-check-certificate https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz -O SOURCES/node-v${NODE_VERSION}-linux-x64.tar.xz
+fi
+
+exit 0
